cc3ede59345ca98d8fda7ae5044cb2fdf873a6d8
Rafal Cycon <rafal.cycon@gmail.com>
2016-08-18 00:45:16 +0200

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo '!!! Joining together files from all Martian years...'
-#python preprocessing/merge_data.py
+python preprocessing/merge_data.py
 
 echo '!!! Creating features'
-#python preprocessing/prepare_dmop.py
-#python preprocessing/prepare_evtf.py
-#python preprocessing/prepare_saaf.py
-#python preprocessing/prepare_ftl.py
+python preprocessing/prepare_dmop.py
+python preprocessing/prepare_evtf.py
+python preprocessing/prepare_saaf.py
+python preprocessing/prepare_ftl.py
 
 echo '!!! Creating datasets'
-#python prepare_data1.py
+python prepare_data1.py
 python prepare_data2.py
